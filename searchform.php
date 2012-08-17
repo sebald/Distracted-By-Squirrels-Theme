34ffec57ac23cd3c1d20624d5f70a34523541385
Sebastian <sebastian.sebald@gmail.com>
2012-08-17 19:37:21 +0200

    starting point remove everything

--- searchform.php
+++
@@ -1,6 +0,0 @@
-<div id="search">
-	<form role="search" method="get" id="searchform" action="<?php bloginfo('url'); ?>">
-		<input type="text" value="" name="s" id="s" />
-		<input type="submit" id="searchsubmit" value="Search" />
-	</form>
-</div>
